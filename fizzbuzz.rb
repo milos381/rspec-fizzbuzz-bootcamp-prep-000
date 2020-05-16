23da4a0bbb95c0e061fff83fbf7d4aa18770c2ec
Milos Popovic <milos_popovic@outlook.com>
2020-05-16 04:53:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num%3 == 0
+    puts "FIZZ"
+  elsif num%5 == 0
+    puts "BUZZ"
+  elsif num%15 == 0
+    puts "FIZZBUZZ"
+  else
+    puts "nil"
+end
